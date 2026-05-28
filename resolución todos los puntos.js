112796448d006b6ffc35a161406f8b4a3b1fa0e9
Alejandro-Palacios <palaciossalee@gmail.com>
2026-05-28 15:19:35 -0300

    se termino el punto 2 y 3

--- resolución todos los puntos.js
+++ resolución todos los puntos.js
@@ -6,3 +6,27 @@ let edad = 18;
 let carrera = "TSDSM";
 let estado = "cursando";
 console.log(`hola yo soy ${nombre}, tengo ${edad} años, estudio en la tecnicatura  ${carrera} y actualmente estoy ${estado}`)
+
+//punto2
+ let nota = 7
+ if (nota>=6){
+    console.log ("aprobado")
+ }
+ else {
+    console.log("desaprobado")
+ }
+
+ //punto 3
+ let edad = 18;
+ if (edad<13){
+    console.log("niño")
+ }
+ else if (edad>=13 && edad<18){
+    console.log("adolescente")
+ }
+ 
+
+
+ 
+ 
+
