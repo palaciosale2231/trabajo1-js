e5701b8c13e949d899a45916e728e2322110b3d3
Alejandro-Palacios <palaciossalee@gmail.com>
2026-05-28 10:40:55 -0300

    se termino el punto 1

--- punto1.js
+++ punto1.js
@@ -1,1 +1,8 @@
-console.log("hola js");
+console.log("hola javascript");
+
+//punto1
+const nombre = "Alejandro Palacios";
+let edad = 18;
+let carrera = "TSDSM";
+let estado = "cursando";
+console.log(`hola yo soy ${nombre}, tengo ${edad} años, estudio en la tecnicatura  ${carrera} y actualmente estoy ${estado}`)
